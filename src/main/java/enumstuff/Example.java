81eecca0ec9dbb3f3720c75b001dca9fb33c3cf9
simon <simon@dancingcloudservices.com>
2023-03-14 08:07:38 -0600

    Day 2 break 2

---
+++ src/main/java/enumstuff/Example.java
@@ -0,0 +1,24 @@
+package enumstuff;
+
+enum X {
+  A, B;
+  int x;
+  public void incrementX() {
+    x++;
+  }
+  // line n1
+//  public String toString() { return name().toUpperCase(); }
+//  @Override public String toString() { return name().toLowerCase(); }
+//  public String name() { return super.name().toUpperCase(); }
+//  public static String name() { return "x.name().toUpperCase()"; }
+}
+
+public class Example {
+  public static void main(String[] args) {
+    System.out.println(X.A);
+    System.out.println(X.A.name());
+
+    X anX = X.valueOf("A");
+    System.out.println(anX);
+  }
+}
